aed9f498d186dbad117339f0156b1ca4391d3918
dianamhl <114951763+dianamhl@users.noreply.github.com>
2023-07-20 12:34:06 +0300

    Add files via upload

---
+++ Workout 8/src/activitati/Rush.java
@@ -0,0 +1,17 @@
+package activitati;
+
+public class Rush implements Activitate {
+
+	@Override
+	public String getDescriere() {
+		// TODO Auto-generated method stub
+		return "Activitate de lucru colaborativ & Q&A"; 
+	}
+
+	@Override
+	public String getDurata() {
+		// TODO Auto-generated method stub
+		return "2h";
+	}
+
+}
